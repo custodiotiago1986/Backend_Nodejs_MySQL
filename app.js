657cfd59a8bb3876f16aeb3d40ed5e2ddc4c3b8c
Tiago <84323433+custodiotiago1986@users.noreply.github.com>
2024-07-31 21:55:46 -0300

    UPDATE CORS Policy

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Adicione esta linha
 const userRoutes = require('./routes/userRoutes');
 const aulasRoutes = require('./routes/aulasRoutes');
 const scriptsRoutes = require('./routes/scriptsRoutes');
@@ -9,6 +10,15 @@ const port = 3000;
 
 // Middleware para parsear o corpo das requisições
 app.use(bodyParser.json());
+
+// Configuração do CORS
+//app.use(cors({
+//origin: 'http://localhost:5500', // Substitua pelo endereço do seu frontend se for diferente
+//  methods: 'GET,POST,PUT,DELETE',
+//allowedHeaders: 'Content-Type,Authorization'
+//}));
+
+app.use(cors()); // Permite todas as origens provisoriamente
 
 // Rotas
 app.use('/users', userRoutes);
